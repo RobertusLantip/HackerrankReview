5f177b7841dbd821ba7f64c4e9fe06ac15e5f4e6
RobertusLantip <RobertusLantip@gmail.com>
2019-05-10 16:56:17 +0800

    Hackerrank review

--- src/main/java/LongestWord.java
+++ src/main/java/LongestWord.java
@@ -3,10 +3,10 @@ public class LongestWord {
         String word = "";
         String[] arrayofword = sentence.split(" ");
         for(int i = 0;i<arrayofword.length;i++){
-            if(arrayofword.length == 0){
-                word = "00";
-            }else if(arrayofword[i].length() %2 == 0 && word.length() < arrayofword[i].length()){
+            if(arrayofword[i].length() %2 == 0 && word.length() < arrayofword[i].length()){
                 word = arrayofword[i];
+            }else if(word == ""){
+                word = "00";
             }
         }
         return word;
